6e0a663cca72f351c1e17b7ec3a3b600e2ed9739
Jeff Schecter <jeffrey.schecter@gmail.com>
2015-10-31 08:48:28 -0700

    start factoring out training and net def

--- train.py
+++ train.py
@@ -2,144 +2,12 @@
 
 import ctypes
 import multiprocessing
-import os
-import random
-import random
 import time
 
 import numpy as np
-import scipy as sp
-
-import lasagne
-import theano
-import theano.tensor as T
-
-import image
-
 from scipy import misc
 
-
-# --------------------------------------------------------------------------- #
-# Utils.                                                                      #
-# --------------------------------------------------------------------------- #
-
-def PrintNetworkShape(net):
-    print "{layer} {inshape} => {outshape}".format(
-        layer=net.__class__.__name__,
-        inshape=getattr(net, "input_shape", ""),
-        outshape=getattr(net, "output_shape", ""))
-    input_layer = getattr(net, "input_layer", None)
-    if input_layer:
-        PrintNetworkShape(input_layer)
-
-
-# --------------------------------------------------------------------------- #
-# Define the network.                                                         #
-# --------------------------------------------------------------------------- #
-  
-def ScaledSigmoid(beta):
-  def Closure(x):
-    return beta * T.nnet.sigmoid(x)
-  return Closure
-
-
-def BuildNet(input_var, height, width):
-  # Inputs are greyscale images.
-  l_in = lasagne.layers.InputLayer(
-      shape=(None, height, width),
-      input_var=input_var)
-
-  # Shuffle them into 1-channel images. 
-  l_inshuf = lasagne.layers.DimshuffleLayer(
-      l_in,
-      (0, 'x', 1, 2))
-  
-  # Apply several convolutional layers, padding at each step to
-  # maintain original image size. We first use a large number
-  # of kernels and ReLUs for feature discovery.
-  l_conv1 = lasagne.layers.Conv2DLayer(
-      l_inshuf,
-      num_filters=12,
-      filter_size=(5, 5),
-      pad="same",
-      nonlinearity=lasagne.nonlinearities.rectify,
-      W=lasagne.init.GlorotUniform())
-  l_conv2 = lasagne.layers.Conv2DLayer(
-      l_conv1,
-      num_filters=5,
-      filter_size=(3, 3),
-      pad="same",
-      nonlinearity=lasagne.nonlinearities.rectify,
-      W=lasagne.init.GlorotUniform())
-  
-  # Last convolutional layer collapses back to 3 kernels, which
-  # should represent luminosity scaling factors for R, G, and B
-  # channels. We use a scaled sigmoid that produces outputs between
-  # 0 and 3, which is what we observe as a typical range of
-  # luminosity scaling factors.
-  l_conv3 = lasagne.layers.Conv2DLayer(
-      l_conv2,
-      num_filters=3,
-      filter_size=(3, 3),
-      pad="same",
-      nonlinearity=ScaledSigmoid(3),
-      W=lasagne.init.GlorotUniform())
-  
-  # Flip the index of the channel so that outputs are in the proper
-  # format for scipy color images: (height, width, rgb)
-  l_outshuf = lasagne.layers.DimshuffleLayer(
-      l_conv3,
-      (0, 2, 3, 1))
-  #l_out = ProportionNormalizationLayer(l_conv3)
-  return l_outshuf
-
-
-def CreateTheanoExprs(height, width, learning_rate):
-  # Our target_var contains raw target images, but we're not actually
-  # training on raw activation values. What we're trying to discover are
-  # scaling factors that need to be applied to greyscale luminosities for
-  # each channel to reconstruct the original image.
-  target_var = T.tensor4("targets")
-  target_ratios = target_var / (target_var.mean(axis=3, keepdims=True) + 1)
-
-  # Inputs are greyscale images, which we can compute from the target full
-  # color images.
-  input_var = (target_var[:, :, :, 0] * 0.299 +
-               target_var[:, :, :, 1] * 0.587 +
-               target_var[:, :, :, 2] * 0.114)
-  
-  # Build network.
-  net = BuildNet(input_var, height, width)
-
-  # Loss expression.
-  # Since we don't have stochastic dropout, we can use the same loss
-  # expr for training and validation. If we want to add a dropout layer,
-  # then we need a separate loss expression for validation where stochastic
-  # elements are explicitly frozen & dropout is disabled.
-  prediction = lasagne.layers.get_output(net)
-  loss = lasagne.objectives.squared_error(prediction, target_ratios).mean()
-
-  # Weight updates during training.
-  params = lasagne.layers.get_all_params(net, trainable=True)
-  updates = lasagne.updates.nesterov_momentum(
-      loss, params, learning_rate=learning_rate, momentum=0.9)
-
-  # Theano function to train a mini-batch.
-  train_fn = theano.function(
-      [target_var],
-      loss,
-      updates=updates,
-      name="Train")
-
-  # Theano function to evaluate / validate on an input.
-  # The difference between this and the training function is that the
-  # test / validation function does not apply weight updates.
-  val_fn = theano.function(
-      [target_var],
-      [prediction, loss],
-      name="Evaluate")
-  
-  return net, train_fn, val_fn
+import image
 
 
 # --------------------------------------------------------------------------- #
@@ -219,7 +87,7 @@ def Minibatches(images, batch_size):
 
 
 def Test(batch_size, test_images, net, val_fn):
-  print "\nTesting!..."
+  print "Testing!..."
   test_errs = []
   iterator = Minibatches(test_images, batch_size)
   mark = time.time()
@@ -236,8 +104,11 @@ def Test(batch_size, test_images, net, val_fn):
 def Train(num_batches, validate_every_n_batches, height, width, batch_size,
           image_handles, val_set_size, test_set_size,
           net, train_fn, val_fn):
-  print "Loading validation and testing images..."
+  # This works with any network and training regimen where the input and
+  # targets are both functions of the same color source image.
+
   # Split image handles in train, test, and validation sets
+  print "Loading validation and testing images..."
   val_images, test_images, train_handles = ValidationTestTrainSplit(
     image_handles, val_set_size, test_set_size, height, width)
 

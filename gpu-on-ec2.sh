28a6fab0a9b00e7c0332cb982104b85f5b52f727
jeffschecter <jeff@levelmoney.com>
2015-11-02 16:20:25 -0800

    stub of gpu ec2 launch script

--- gpu-on-ec2.sh
+++ gpu-on-ec2.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-ami="ami-b36981d8"
 # http://markus.com/install-theano-on-aws/
 # has Theano and CUDA pre-installed I think?
-ami="ami-b141a2f5"
+AMI="ami-b141a2f5"
+TYPE="g2.2xlarge"
+
+aws ec2 run-instances \
+--region us-west-1 \
+--image-id $AMI \
+--count 1 \
+--instance-type $TYPE \
+--key-name theano \
+--security-groups theano
+
+# Then ssh tino ubuntu@
